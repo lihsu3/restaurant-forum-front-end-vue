1a263b8fb3f489c96a4e85139b02ed1a913d7c19
lihsu <lihsu3@gmail.com>
2020-02-28 14:28:24 +0800

    102 - install moment and change datetime format

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -15,7 +15,7 @@
           <!-- <small>{{ restaurant.Category.name }}</small> -->
         </h4>
         <p>{{ restaurant.description }}</p>
-        {{ restaurant.createdAt }}
+        {{ restaurant.createdAt | fromNow }}
         <hr>
       </div>
     </div>
@@ -23,7 +23,17 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
+  filters: {
+    fromNow (datetime) {
+      if (!datetime) {
+        return '-'
+      } 
+      return moment(datetime).fromNow()
+    }
+  },
   props: {
     restaurants: {
       type: Array,
