490705b46a95c2b8e946bdeb81edba359a4ea286
lihsu <lihsu3@gmail.com>
2020-02-28 14:11:50 +0800

    101 - display newest restaurants; a bug to be fixed

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -9,7 +9,7 @@
       <div class="col-md-6">
         <h3>最新餐廳</h3>
         <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants />
+        <NewestRestaurants v-bind:restaurants="restaurants" />
       </div>
       <div class="col-md-6">
         <!-- 最新評論 NewestComments-->
@@ -25,181 +25,181 @@ import NewestRestaurants from '../components/NewestRestaurants'
 
 const dummyData = {
   "restaurants": [
-      {
-          "id": 144,
-          "name": "麥當當",
-          "tel": "123",
-          "address": "麥當勞總部",
-          "opening_hours": "03:33",
-          "description": "",
-          "image": null,
-          "viewCounts": null,
-          "createdAt": "2020-02-16T05:09:24.297Z",
-          "updatedAt": "2020-02-16T05:09:24.297Z",
-          "CategoryId": 6,
-          "Category": {
-              "id": 6,
-              "name": "美式料理",
-              "createdAt": "2019-11-20T06:25:42.917Z",
-              "updatedAt": "2019-11-20T06:25:42.917Z"
-          }
-      },
-      {
-          "id": 143,
-          "name": "testrestaurant",
-          "tel": "",
-          "address": "",
-          "opening_hours": "",
-          "description": "",
-          "image": null,
-          "viewCounts": null,
-          "createdAt": "2020-01-19T03:04:21.188Z",
-          "updatedAt": "2020-01-19T03:04:21.188Z",
-          "CategoryId": 109,
-          "Category": {
-              "id": 109,
-              "name": "cool",
-              "createdAt": "2019-12-17T11:36:08.937Z",
-              "updatedAt": "2019-12-17T11:36:08.937Z"
-          }
-      },
-      {
-          "id": 142,
-          "name": "test1233",
-          "tel": "",
-          "address": "",
-          "opening_hours": "",
-          "description": "",
-          "image": null,
-          "viewCounts": null,
-          "createdAt": "2020-01-12T11:58:30.114Z",
-          "updatedAt": "2020-01-12T11:58:30.114Z",
-          "CategoryId": 7,
-          "Category": {
-              "id": 7,
-              "name": "複合式料理",
-              "createdAt": "2019-11-20T06:25:42.917Z",
-              "updatedAt": "2019-11-20T06:25:42.917Z"
-          }
-      },
-      {
-          "id": 109,
-          "name": "asd",
-          "tel": "083-343-7765",
-          "address": "",
-          "opening_hours": "",
-          "description": "",
-          "image": null,
-          "viewCounts": null,
-          "createdAt": "2020-01-08T14:03:07.273Z",
-          "updatedAt": "2020-01-08T14:03:07.273Z",
-          "CategoryId": 109,
-          "Category": {
-              "id": 109,
-              "name": "cool",
-              "createdAt": "2019-12-17T11:36:08.937Z",
-              "updatedAt": "2019-12-17T11:36:08.937Z"
-          }
-      },
-      {
-          "id": 105,
-          "name": "testv",
-          "tel": "020002222",
-          "address": "st.Road",
-          "opening_hours": "01:00",
-          "description": "",
-          "image": "https://i.imgur.com/7ZpjPIA.jpg",
-          "viewCounts": null,
-          "createdAt": "2019-12-29T04:53:35.616Z",
-          "updatedAt": "2019-12-29T04:53:35.616Z",
-          "CategoryId": 7,
-          "Category": {
-              "id": 7,
-              "name": "複合式料理",
-              "createdAt": "2019-11-20T06:25:42.917Z",
-              "updatedAt": "2019-11-20T06:25:42.917Z"
-          }
-      },
-      {
-          "id": 104,
-          "name": "餐廳種子資料都沒category",
-          "tel": "020002222",
-          "address": "st.Road",
-          "opening_hours": "00:00",
-          "description": "",
-          "image": "https://i.imgur.com/9b88oCl.jpg",
-          "viewCounts": null,
-          "createdAt": "2019-12-29T04:12:02.664Z",
-          "updatedAt": "2019-12-29T04:12:02.664Z",
-          "CategoryId": 7,
-          "Category": {
-              "id": 7,
-              "name": "複合式料理",
-              "createdAt": "2019-11-20T06:25:42.917Z",
-              "updatedAt": "2019-11-20T06:25:42.917Z"
-          }
-      },
-      {
-          "id": 103,
-          "name": "testvvvv",
-          "tel": "020002222",
-          "address": "st.Road",
-          "opening_hours": "00:00",
-          "description": "",
-          "image": "https://i.imgur.com/nsmu4r6.jpg",
-          "viewCounts": null,
-          "createdAt": "2019-12-29T04:10:12.163Z",
-          "updatedAt": "2019-12-29T04:10:12.163Z",
-          "CategoryId": 6,
-          "Category": {
-              "id": 6,
-              "name": "美式料理",
-              "createdAt": "2019-11-20T06:25:42.917Z",
-              "updatedAt": "2019-11-20T06:25:42.917Z"
-          }
-      },
-      {
-          "id": 99,
-          "name": "cool",
-          "tel": "457291867",
-          "address": "26 Bluegrass Street",
-          "opening_hours": "13:02",
-          "description": "coolcoolcool",
-          "image": "https://i.imgur.com/OC8nekt.png",
-          "viewCounts": null,
-          "createdAt": "2019-12-09T02:50:06.794Z",
-          "updatedAt": "2019-12-09T02:50:06.794Z",
-          "CategoryId": null,
-          "Category": null
-      },
-      {
-          "id": 97,
-          "name": "DGGSXFG",
-          "tel": "04534545",
-          "address": "XDFGZSDFGZJFZFGAD",
-          "opening_hours": "12:59",
-          "description": "XDFGBSZFDGZZSDFGsdg",
-          "image": "https://i.imgur.com/QOX4UzQ.jpg",
-          "viewCounts": null,
-          "createdAt": "2019-12-05T09:11:37.628Z",
-          "updatedAt": "2019-12-05T09:11:37.628Z",
-          "CategoryId": null,
-          "Category": null
-      },
-      {
-          "id": 96,
-          "name": "dfgdfhgdfh xfghfgxh",
-          "tel": "xfgxgfbxfb",
-          "address": "dzfbdfzbzfbzs",
-          "opening_hours": "12:59",
-          "description": "AFAScsddggrhHFJSGFJFDGsdF",
-          "image": "https://i.imgur.com/oOHdiBw.jpg",
-          "viewCounts": null,
-          "createdAt": "2019-12-05T09:11:11.111Z",
-          "updatedAt": "2019-12-05T09:11:11.111Z",
-          "CategoryId": null,
-          "Category": null
+    {
+      "id": 144,
+      "name": "麥當當",
+      "tel": "123",
+      "address": "麥當勞總部",
+      "opening_hours": "03:33",
+      "description": "",
+      "image": null,
+      "viewCounts": null,
+      "createdAt": "2020-02-16T05:09:24.297Z",
+      "updatedAt": "2020-02-16T05:09:24.297Z",
+      "CategoryId": 6,
+      "Category": {
+        "id": 6,
+        "name": "美式料理",
+        "createdAt": "2019-11-20T06:25:42.917Z",
+        "updatedAt": "2019-11-20T06:25:42.917Z"
       }
+    },
+    {
+      "id": 143,
+      "name": "testrestaurant",
+      "tel": "",
+      "address": "",
+      "opening_hours": "",
+      "description": "",
+      "image": null,
+      "viewCounts": null,
+      "createdAt": "2020-01-19T03:04:21.188Z",
+      "updatedAt": "2020-01-19T03:04:21.188Z",
+      "CategoryId": 109,
+      "Category": {
+        "id": 109,
+        "name": "cool",
+        "createdAt": "2019-12-17T11:36:08.937Z",
+        "updatedAt": "2019-12-17T11:36:08.937Z"
+      }
+    },
+    {
+      "id": 142,
+      "name": "test1233",
+      "tel": "",
+      "address": "",
+      "opening_hours": "",
+      "description": "",
+      "image": null,
+      "viewCounts": null,
+      "createdAt": "2020-01-12T11:58:30.114Z",
+      "updatedAt": "2020-01-12T11:58:30.114Z",
+      "CategoryId": 7,
+      "Category": {
+        "id": 7,
+        "name": "複合式料理",
+        "createdAt": "2019-11-20T06:25:42.917Z",
+        "updatedAt": "2019-11-20T06:25:42.917Z"
+      }
+    },
+    {
+      "id": 109,
+      "name": "asd",
+      "tel": "083-343-7765",
+      "address": "",
+      "opening_hours": "",
+      "description": "",
+      "image": null,
+      "viewCounts": null,
+      "createdAt": "2020-01-08T14:03:07.273Z",
+      "updatedAt": "2020-01-08T14:03:07.273Z",
+      "CategoryId": 109,
+      "Category": {
+        "id": 109,
+        "name": "cool",
+        "createdAt": "2019-12-17T11:36:08.937Z",
+        "updatedAt": "2019-12-17T11:36:08.937Z"
+      }
+    },
+    {
+        "id": 105,
+        "name": "testv",
+        "tel": "020002222",
+        "address": "st.Road",
+        "opening_hours": "01:00",
+        "description": "",
+        "image": "https://i.imgur.com/7ZpjPIA.jpg",
+        "viewCounts": null,
+        "createdAt": "2019-12-29T04:53:35.616Z",
+        "updatedAt": "2019-12-29T04:53:35.616Z",
+        "CategoryId": 7,
+        "Category": {
+            "id": 7,
+            "name": "複合式料理",
+            "createdAt": "2019-11-20T06:25:42.917Z",
+            "updatedAt": "2019-11-20T06:25:42.917Z"
+        }
+    },
+    {
+        "id": 104,
+        "name": "餐廳種子資料都沒category",
+        "tel": "020002222",
+        "address": "st.Road",
+        "opening_hours": "00:00",
+        "description": "",
+        "image": "https://i.imgur.com/9b88oCl.jpg",
+        "viewCounts": null,
+        "createdAt": "2019-12-29T04:12:02.664Z",
+        "updatedAt": "2019-12-29T04:12:02.664Z",
+        "CategoryId": 7,
+        "Category": {
+            "id": 7,
+            "name": "複合式料理",
+            "createdAt": "2019-11-20T06:25:42.917Z",
+            "updatedAt": "2019-11-20T06:25:42.917Z"
+        }
+    },
+    {
+        "id": 103,
+        "name": "testvvvv",
+        "tel": "020002222",
+        "address": "st.Road",
+        "opening_hours": "00:00",
+        "description": "",
+        "image": "https://i.imgur.com/nsmu4r6.jpg",
+        "viewCounts": null,
+        "createdAt": "2019-12-29T04:10:12.163Z",
+        "updatedAt": "2019-12-29T04:10:12.163Z",
+        "CategoryId": 6,
+        "Category": {
+            "id": 6,
+            "name": "美式料理",
+            "createdAt": "2019-11-20T06:25:42.917Z",
+            "updatedAt": "2019-11-20T06:25:42.917Z"
+        }
+    },
+    {
+        "id": 99,
+        "name": "cool",
+        "tel": "457291867",
+        "address": "26 Bluegrass Street",
+        "opening_hours": "13:02",
+        "description": "coolcoolcool",
+        "image": "https://i.imgur.com/OC8nekt.png",
+        "viewCounts": null,
+        "createdAt": "2019-12-09T02:50:06.794Z",
+        "updatedAt": "2019-12-09T02:50:06.794Z",
+        "CategoryId": null,
+        "Category": null
+    },
+    {
+        "id": 97,
+        "name": "DGGSXFG",
+        "tel": "04534545",
+        "address": "XDFGZSDFGZJFZFGAD",
+        "opening_hours": "12:59",
+        "description": "XDFGBSZFDGZZSDFGsdg",
+        "image": "https://i.imgur.com/QOX4UzQ.jpg",
+        "viewCounts": null,
+        "createdAt": "2019-12-05T09:11:37.628Z",
+        "updatedAt": "2019-12-05T09:11:37.628Z",
+        "CategoryId": null,
+        "Category": null
+    },
+    {
+        "id": 96,
+        "name": "dfgdfhgdfh xfghfgxh",
+        "tel": "xfgxgfbxfb",
+        "address": "dzfbdfzbzfbzs",
+        "opening_hours": "12:59",
+        "description": "AFAScsddggrhHFJSGFJFDGsdF",
+        "image": "https://i.imgur.com/oOHdiBw.jpg",
+        "viewCounts": null,
+        "createdAt": "2019-12-05T09:11:11.111Z",
+        "updatedAt": "2019-12-05T09:11:11.111Z",
+        "CategoryId": null,
+        "Category": null
+    }
   ],
   "comments": [
       {
@@ -451,7 +451,7 @@ export default {
   data () {
     return {
       restaurants: [],
-      comments: []
+      comments: [],
     }
   },
   created () {
