402baad0e0d5a6c27bef2ebfea2a04d0fc96d638
lihsu <lihsu3@gmail.com>
2020-02-29 11:04:31 +0800

    106 - add RestaurantsNavPills

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <NavTabs />
     <!-- 餐廳類別標籤 RestaurantsNavPills -->
+    <RestaurantsNavPills :categories="categories"/>
 
     <div class="row">
       <!-- 餐廳卡片 RestaurantCard-->
@@ -19,6 +20,7 @@
 <script>
 import NavTabs from '../components/NavTabs'
 import RestaurantCard from '../components/RestaurantCard'
+import RestaurantsNavPills from '../components/RestaurantsNavPills'
 
 const dummyData = {
     "restaurants": [
@@ -246,7 +248,8 @@ const dummyData = {
 export default {
   components: {
     NavTabs,
-    RestaurantCard
+    RestaurantCard,
+    RestaurantsNavPills
   },
   data () {
     return {
