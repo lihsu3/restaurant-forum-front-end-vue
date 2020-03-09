5cc3654bcb465351a1a97b313891d0e937d43890
lihsu <lihsu3@gmail.com>
2020-03-10 05:36:55 +0800

    112 - delete a comment

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -5,7 +5,10 @@
     <RestaurantDetail :initial-restaurant="restaurant"/>
     <hr>
     <!-- 餐廳評論 RestaurantComments -->
-    <RestaurantComments :restaurant-comments="restaurantComments"/>
+    <RestaurantComments 
+      :restaurant-comments="restaurantComments"
+      @after-delete-comment="afterDeleteComment"
+    />
     <!-- 新增評論 CreateComment -->
   </div>
 </template>
@@ -104,6 +107,12 @@ export default {
       }
 
       this.restaurantComments = dummyData.restaurant.Comments
+    },
+    afterDeleteComment (commentId) {
+      // 以 filter 保留未被選擇的 comment.id
+      this.restaurantComments = this.restaurantComments.filter(
+        comment => comment.id !== commentId
+      )
     }
   }
 }
