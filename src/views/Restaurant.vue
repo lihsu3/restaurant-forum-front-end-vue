a53218f7c8cc83d876fdb59033c3d624ecf8249e
lihsu <lihsu3@gmail.com>
2020-03-10 05:04:45 +0800

    111 - restaurant comments and mixins

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -5,12 +5,14 @@
     <RestaurantDetail :initial-restaurant="restaurant"/>
     <hr>
     <!-- 餐廳評論 RestaurantComments -->
+    <RestaurantComments :restaurant-comments="restaurantComments"/>
     <!-- 新增評論 CreateComment -->
   </div>
 </template>
 
 <script>
 import RestaurantDetail from '../components/RestaurantDetail'
+import RestaurantComments from '../components/RestaurantComments'
 
 const dummyData = {
   "restaurant": {
@@ -59,7 +61,8 @@ const dummyData = {
 
 export default {
   components: {
-    RestaurantDetail
+    RestaurantDetail,
+    RestaurantComments
   },
   data () {
     return {
