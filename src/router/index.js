c3aa59b6dc62216defcbaaddf6d44de7fbe5d1af
lihsu <lihsu3@gmail.com>
2020-03-19 14:31:04 +0800

    121 - create and delete categories

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,11 @@ const routes = [
     component: () => import('../views/AdminRestaurant.vue')
   },
   {
+    path: '/admin/categories',
+    name: 'admin-categories',
+    component: () => import('../views/AdminCategories.vue')
+  },
+  {
     path: '/signin',
     name: 'sign-in',
     component: SignIn
